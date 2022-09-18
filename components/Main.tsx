f7f294cfd4f1d364b42b13b8ae45991278de8644
toda <starfish719@gmail.com>
2022-09-18 15:57:55 +0900

    feat: 🎸 set inv val

--- components/Main.tsx
+++ components/Main.tsx
@@ -162,11 +162,7 @@ export const Main = () => {
                           autoComplete="individual_value"
                           className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                         >
-                          <option value={31}>31</option>
-                          <option value={30}>30</option>
-                          <option value={2}>2</option>
-                          <option value={1}>1</option>
-                          <option value={0}>0</option>
+                          {[...Array(32).keys()].map((_, i) => (<option key={i} value={i}>{i}</option>)).reverse()}
                         </select>
                       </div>
 
