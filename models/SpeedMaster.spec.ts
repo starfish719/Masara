196e0379524d1df60467dab3a37f93b431318c8f
starfish719 <starfish719@gmail.com>
2023-01-14 12:09:01 +0900

    add test case

--- models/SpeedMaster.spec.ts
+++ models/SpeedMaster.spec.ts
@@ -165,6 +165,156 @@ describe('speedResults', () => {
     ]);
   });
 
+  it('when baseStats is 95 and personality is high, return result list', () => {
+    expect(speedResults({
+      pokemon: {
+        value: 'hoge',
+        label: 'fuga',
+        baseStats: 95,
+      },
+      individualValue: 31,
+      personality: 1.1,
+      rank: 0,
+      item: 1.0,
+      ability: 1.0,
+      isTailwind: false,
+      isParalysis: false,
+    })).toEqual([
+      {
+        effortValue: 0,
+        resultValue: 126,
+      },
+      {
+        effortValue: 4,
+        resultValue: 127,
+      },
+      {
+        effortValue: 12,
+        resultValue: 128,
+      },
+      {
+        effortValue: 20,
+        resultValue: 129,
+      },
+      {
+        effortValue: 28,
+        resultValue: 130,
+      },
+      {
+        effortValue: 36,
+        resultValue: 132,
+      },
+      {
+        effortValue: 44,
+        resultValue: 133,
+      },
+      {
+        effortValue: 52,
+        resultValue: 134,
+      },
+      {
+        effortValue: 60,
+        resultValue: 135,
+      },
+      {
+        effortValue: 68,
+        resultValue: 136,
+      },
+      {
+        effortValue: 76,
+        resultValue: 137,
+      },
+      {
+        effortValue: 84,
+        resultValue: 138,
+      },
+      {
+        effortValue: 92,
+        resultValue: 139,
+      },
+      {
+        effortValue: 100,
+        resultValue: 140,
+      },
+      {
+        effortValue: 108,
+        resultValue: 141,
+      },
+      {
+        effortValue: 116,
+        resultValue: 143,
+      },
+      {
+        effortValue: 124,
+        resultValue: 144,
+      },
+      {
+        effortValue: 132,
+        resultValue: 145,
+      },
+      {
+        effortValue: 140,
+        resultValue: 146,
+      },
+      {
+        effortValue: 148,
+        resultValue: 147,
+      },
+      {
+        effortValue: 156,
+        resultValue: 148,
+      },
+      {
+        effortValue: 164,
+        resultValue: 149,
+      },
+      {
+        effortValue: 172,
+        resultValue: 150,
+      },
+      {
+        effortValue: 180,
+        resultValue: 151,
+      },
+      {
+        effortValue: 188,
+        resultValue: 152,
+      },
+      {
+        effortValue: 196,
+        resultValue: 154,
+      },
+      {
+        effortValue: 204,
+        resultValue: 155,
+      },
+      {
+        effortValue: 212,
+        resultValue: 156,
+      },
+      {
+        effortValue: 220,
+        resultValue: 157,
+      },
+      {
+        effortValue: 228,
+        resultValue: 158,
+      },
+      {
+        effortValue: 236,
+        resultValue: 159,
+      },
+      {
+        effortValue: 244,
+        resultValue: 160,
+      },
+      {
+        effortValue: 252,
+        resultValue: 161,
+      },
+    ]);
+  });
+
   describe('calcSpeed', () => {
     it('return speed result value', () => {
       const mockCalcProps = {
